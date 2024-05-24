8cad1d5d64a0fcf170cf41109b4e86c2433ed6df
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-05-24 11:11:55 +0200

    Correct home manager configuration

--- flake.nix
+++ flake.nix
@@ -4,13 +4,24 @@
   inputs = {
     # NixOS official package source, hopefully the most recent stable version
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.oreb = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./nixos/configuration.nix
+	home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.pentaxis93 = import ./home.nix;
+        }
       ];
     };
   };
