7f0b8c5a148058657ecc7251f1055b03884b8d0b
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-05-24 10:06:15 +0200

    Setup home manager

---
+++ home.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+
+  # Basic user settings
+  home.username = "pentaxis93";
+  home.homeDirectory = "/home/pentaxis93";
+
+  # User-specific packages
+  home.packages = with pkgs; [
+    cowsay	# Talking cow
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "pentaxis93";
+    userEmail = "13393192+pentaxis93@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  # Set environment variables
+  home.sessionVariables = {
+    FLAKE = "/home/pentaxis93/.dotfiles";
+  };
+
+  # Do not change - this is the originally installed version for backwards compatibility
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+}
