ce984ca140c5f880d9cb9776b2f9e645a4fb70b0
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-05-26 12:05:32 +0200

    Use nixos stable version 24.05

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     # NixOS official package source, hopefully the most recent stable version
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
