abccd242087b65865adcdfe54d512944c90f49db
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-05-27 16:32:28 +0200

    Add conform-nvim config (not working)

--- nixos/configuration.oreb.nix
+++ nixos/configuration.oreb.nix
@@ -106,6 +106,29 @@
       
       comment.enable = true;
 
+      conform-nvim = {
+	enable = true;
+	# Map of filetype to formatters
+	formattersByFt = {
+	  lua = [ "stylua" ];
+	  # Conform will run multiple formatters sequentially
+	  python = [ "isort" "black" ];
+	  # Use a sub-list to run only the first available formatter
+	  javascript = [ [ "prettierd" "prettier" ] ];
+	  # Use the "*" filetype to run formatters on all filetypes.
+	  "*" = [ "codespell" ];
+	  # Use the "_" filetype to run formatters on filetypes that don't
+	  # have other formatters configured.
+	  "_" = [ "trim_whitespace" ];
+	 };
+	formatOnSave = ''
+	  require("conform").format({
+	    async = true,
+	    timeout_ms = 1000,
+	  })
+	'';
+      };
+
       gitsigns.enable = true;
 
       flash.enable = true;
