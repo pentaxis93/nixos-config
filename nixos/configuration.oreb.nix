d0eeb972ed4d3b7a23dc13b15203aca7380a3ed2
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-05-30 15:17:35 +0200

    Upgrade to KDE Plasma 6 with wayland

--- nixos/configuration.oreb.nix
+++ nixos/configuration.oreb.nix
@@ -115,7 +115,8 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
