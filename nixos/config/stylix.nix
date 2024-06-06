8acb7ee2eefbb35f5cde19cf98252e0363f2efa1
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-06-06 22:51:55 +0200

    Apply dracula theme

--- nixos/config/stylix.nix
+++ nixos/config/stylix.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   stylix = {
     autoEnable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
     image = pkgs.fetchurl {
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
