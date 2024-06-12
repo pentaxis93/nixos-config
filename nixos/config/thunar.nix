c41310159e0e2a7c3aaca5c951cf8d88ee41f99b
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-06-12 08:42:58 +0200

    Add support for file compression and extraction

--- nixos/config/thunar.nix
+++ nixos/config/thunar.nix
@@ -9,7 +9,20 @@
     };
   };
 
+  # Add support for compressed files
   environment.systemPackages = with pkgs; [
     xarchiver # Compress and decompress files
+    p7zip # handle 7z files
+    unrar # handle rar files
+    zip # handle zip files
+    unzip # handle zip files
+    gzip # handle gz files
+    bzip2 # handle bz2 files
+    gnutar # handle tar files
+    xz # handle xz files
+    lrzip # handle lrz files
+    arj # handle arj files
+    lha # handle lha files
+    cabextract # handle cab files
   ];
 }
