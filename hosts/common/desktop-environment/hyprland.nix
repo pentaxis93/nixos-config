b922003458016c44b0a54d97547bac8a062b112e
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-06-15 18:29:05 +0200

    Add desktop portals to hyprland config
    These add certain basic functionality to the desktop, like the ability
    to screen share.

--- hosts/common/desktop-environment/hyprland.nix
+++ hosts/common/desktop-environment/hyprland.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -9,6 +10,10 @@
 
   config = lib.mkIf config.hyprland.enable {
     programs.hyprland.enable = true;
-    # xdg.portal.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+    ];
   };
 }
