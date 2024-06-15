0e8b676c010310e4f9d78de01b81066db48f10b5
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-06-15 18:02:56 +0200

    Enable stylix
    This adds color and cursor themes to every app that has these options.

--- hosts/common/desktop-environment/stylix.nix
+++ hosts/common/desktop-environment/stylix.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   stylix = {
+    enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
     image = pkgs.fetchurl {
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
