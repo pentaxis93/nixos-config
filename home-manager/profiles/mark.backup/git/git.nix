707b7cabd2aaa41c17e137778649916a6bdd81bd
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-06-15 11:44:33 +0200

    Remove unnecessary backup directories

--- home-manager/profiles/mark.backup/git/git.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  programs.git = {
-    userName = "CatsAreCuteAndFast";
-    userEmail = "132775888+CatsAreCuteAndFast@users.noreply.github.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
-}
