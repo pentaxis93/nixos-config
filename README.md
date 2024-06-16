6820e758d83bacaa21e7c39d480d8238c9aee336
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-06-16 18:06:00 +0200

    Add nvtop

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # TODO
 
 - [ ] Add gpu monitor tools
-- [ ] Add kittysay
 - [x] Set theme for rofi
--- hosts/common/desktop-apps/default.nix
+++ hosts/common/desktop-apps/default.nix
@@ -7,6 +7,7 @@
   ];
 
   environment.systemPackages = with pkgs; [
+    nvtop # GPU monitor
     vlc # Video player
   ];
 }
