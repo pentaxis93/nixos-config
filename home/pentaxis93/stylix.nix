68b8ab4f81ff788605db5d49d0e50fcf70c11ebd
CatsAreCuteAndFast <132775888+CatsAreCuteAndFast@users.noreply.github.com>
2024-07-13 20:06:08 +0200

    Removed stylix

--- home/pentaxis93/stylix.nix
+++ home/pentaxis93/stylix.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   stylix = {
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    # image = pkgs.fetchurl {
+    #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+    #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    # };
   };
 }
